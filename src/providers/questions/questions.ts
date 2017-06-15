bfceb48140caece7693e15e6a811cfb177dcaf77
Jongmin Kim <kjm141215@gmail.com>
2017-06-14 20:36:55 -0700

    Retrieve questions from the backend

--- src/providers/questions/questions.ts
+++ src/providers/questions/questions.ts
@@ -2,17 +2,18 @@ import { Injectable } from '@angular/core';
 import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
 
-/*
-  Generated class for the QuestionsProvider provider.
-
-  See https://angular.io/docs/ts/latest/guide/dependency-injection.html
-  for more info on providers and Angular 2 DI.
-*/
 @Injectable()
 export class QuestionsProvider {
 
   constructor(public http: Http) {
     console.log('Hello QuestionsProvider Provider');
+  }
+  baseUrl: string = "http://localhost:3000/api";
+  path: string = "/Questions"
+
+  getQuestions(token) {
+    console.log('inside getQuestions');
+    return this.http.get(this.baseUrl + this.path + '?access_token=' + token);
   }
 
 }
